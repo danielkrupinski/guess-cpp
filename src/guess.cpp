bd63b9f6a49800f2b622938db2abb5c2a6fd62b0
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-17 19:08:43 +0100

    Add first guess instruction

--- src/guess.cpp
+++ src/guess.cpp
@@ -4,4 +4,8 @@ void guess()
 {
     std::cout << "Think of a number between 1 and 100\n";
     std::cout << "I will try to guess it\n";
+    srand(time(NULL));
+    int guess {rand()%100+1};
+    std::cout << "Is your number greater than " << guess << '?';
+    std::cout << "(Y)es/(N)o";
 }
