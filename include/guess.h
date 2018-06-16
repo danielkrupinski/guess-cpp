1778fe2e7d875cfa748844f1efa231a2caf97956
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-16 20:27:11 +0200

    Update indentation

--- include/guess.h
+++ include/guess.h
@@ -4,19 +4,19 @@
 #include <iostream>
 
 class Guess {
-    private:
-        int guess;
-        int high;
-        int low;
-    public:
-        bool in_progress;
-        Guess();
-        void init();
-        void ask();
-        void answer();
-        void too_high();
-        void too_low();
-        void equal();
+private:
+    int guess;
+    int high;
+    int low;
+public:
+    bool in_progress;
+    Guess();
+    void init();
+    void ask();
+    void answer();
+    void too_high();
+    void too_low();
+    void equal();
 };
 
 #endif
