c81ecb82fc807302c313d88f5ccdaefc1931c288
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-16 20:28:40 +0200

    Do not use dynamic memory allocation for guess object

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,11 @@
 
 int main()
 {
-    Guess* guess = new Guess;
-    while (guess->in_progress)
+    Guess guess;
+    while (guess.in_progress)
     {
-        guess->ask();
-        guess->answer();
+        guess.ask();
+        guess.answer();
     }
-    delete guess;
 	return 0;
 }
